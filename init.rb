4ea7c6910459d2b208b5d4c93ee7c44685edce87
Jakob Skjerning <jakob@mentalized.net>
2009-08-02 16:16:13 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :redmine_github_hook do
+  name 'Redmine Github Hook plugin'
+  author 'Jakob Skjerning'
+  description 'This plugin allows Redmine to receive and act on post-receive notifications from Github <http://github.com>'
+  version '0.0.1'
+end
