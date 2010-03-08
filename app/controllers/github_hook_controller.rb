a55623f33243c6076727181097d9951b886d03d3
Nathan L Smith <nlloyds@gmail.com>
2010-03-07 22:06:01 -0600

    downcase identifier

--- app/controllers/github_hook_controller.rb
+++ app/controllers/github_hook_controller.rb
@@ -11,7 +11,7 @@ class GithubHookController < ApplicationController
     # For now, we assume that the repository name is the same as the project identifier
     identifier = payload['repository']['name']
 
-    project = Project.find_by_identifier(identifier)
+    project = Project.find_by_identifier(identifier.downcase)
     raise ActiveRecord::RecordNotFound, "No project found with identifier '#{identifier}'" if project.nil?
     
     repository = project.repository
