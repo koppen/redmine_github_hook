12be857e1b7396b8c90f991b84515af9234de6d9
Marvin Reimer <therealmarv@googlemail.com>
2012-05-15 16:09:42 +0200

    convert nasty tabs to spaces

--- app/controllers/github_hook_controller.rb
+++ app/controllers/github_hook_controller.rb
@@ -52,17 +52,17 @@ class GithubHookController < ApplicationController
   # Fetches updates from the remote repository
   def update_repository(repository)
     all_branches = Setting.plugin_redmine_github_hook[:all_branches]
-	all_branches = false if not all_branches
+    all_branches = false if not all_branches
     if all_branches != "yes"
-	  command = git_command('fetch --all', repository)
+      command = git_command('fetch --all', repository)
       exec(command)
-	else
+    else
       command = git_command('fetch origin', repository)
       if exec(command)
         command = git_command("fetch origin '+refs/heads/*:refs/heads/*'", repository)
         exec(command)
       end
-	end
+    end
   end
 
   # Gets the project identifier from the querystring parameters and if that's not supplied, assume
