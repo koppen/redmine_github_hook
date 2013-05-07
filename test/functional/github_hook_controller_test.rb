813af6866cceb09283af9392db7fc9dd0f7d3900
Jakob Skjerning <jakob@mentalized.net>
2013-05-07 09:39:18 +0200

    Less instance variables for more predictable tests

--- test/functional/github_hook_controller_test.rb
+++ test/functional/github_hook_controller_test.rb
@@ -4,10 +4,9 @@ require 'test/unit'
 require 'mocha'
 
 class GithubHookControllerTest < ActionController::TestCase
-
-  def setup
+  def json
     # Sample JSON post from http://github.com/guides/post-receive-hooks
-    @json = '{ 
+    '{
       "before": "5aef35982fb2d34e9d9d4502f6ede1072793222d",
       "repository": {
         "url": "http://github.com/defunkt/github",
@@ -47,14 +46,26 @@ class GithubHookControllerTest < ActionController::TestCase
       "after": "de8251ff97ee194a289832576287d6f8ad74e3d0",
       "ref": "refs/heads/master"
     }'
+  end
 
-    @repository = Repository::Git.new
+  def repository
+    return @repository if @repository
+
+    @repository ||= Repository::Git.new
     @repository.stubs(:fetch_changesets).returns(true)
+    @repository
+  end
 
-    @project = Project.new
-    @project.repositories << @repository
+  def project
+    return @project if @project
 
-    Project.stubs(:find_by_identifier).with('github').returns(@project)
+    @project ||= Project.new
+    @project.repositories << repository
+    @project
+  end
+
+  def setup
+    Project.stubs(:find_by_identifier).with('github').returns(project)
 
     # Make sure we don't run actual commands in test
     @controller.expects(:system).never
@@ -68,48 +79,48 @@ class GithubHookControllerTest < ActionController::TestCase
   end
 
   def do_post(payload = nil)
-    payload = @json if payload.nil?
+    payload = json if payload.nil?
     payload = payload.to_json if payload.is_a?(Hash)
     post :index, :payload => payload
   end
 
   def test_should_use_the_repository_name_as_project_identifier
-    Project.expects(:find_by_identifier).with('github').returns(@project)
+    Project.expects(:find_by_identifier).with('github').returns(project)
     @controller.stubs(:exec).returns(true)
     do_post
   end
 
   def test_should_fetch_changes_from_origin
-    Project.expects(:find_by_identifier).with('github').returns(@project)
-    @controller.expects(:exec).with("git --git-dir='#{@repository.url}' fetch origin")
+    Project.expects(:find_by_identifier).with('github').returns(project)
+    @controller.expects(:exec).with("git --git-dir='#{repository.url}' fetch origin")
     do_post
   end
 
   def test_should_reset_repository_when_fetch_origin_succeeds
-    Project.expects(:find_by_identifier).with('github').returns(@project)
-    @controller.expects(:exec).with("git --git-dir='#{@repository.url}' fetch origin").returns(true)
-    @controller.expects(:exec).with("git --git-dir='#{@repository.url}' fetch origin '+refs/heads/*:refs/heads/*'")
+    Project.expects(:find_by_identifier).with('github').returns(project)
+    @controller.expects(:exec).with("git --git-dir='#{repository.url}' fetch origin").returns(true)
+    @controller.expects(:exec).with("git --git-dir='#{repository.url}' fetch origin '+refs/heads/*:refs/heads/*'")
     do_post
   end
 
   def test_should_not_reset_repository_when_fetch_origin_fails
-    Project.expects(:find_by_identifier).with('github').returns(@project)
-    @controller.expects(:exec).with("git --git-dir='#{@repository.url}' fetch origin").returns(false)
-    @controller.expects(:exec).with("git --git-dir='#{@repository.url}' reset --soft refs\/remotes\/origin\/master").never
+    Project.expects(:find_by_identifier).with('github').returns(project)
+    @controller.expects(:exec).with("git --git-dir='#{repository.url}' fetch origin").returns(false)
+    @controller.expects(:exec).with("git --git-dir='#{repository.url}' reset --soft refs\/remotes\/origin\/master").never
     do_post
   end
 
   def test_should_use_project_identifier_from_request
-    Project.expects(:find_by_identifier).with('redmine').returns(@project)
+    Project.expects(:find_by_identifier).with('redmine').returns(project)
     @controller.stubs(:exec).returns(true)
-    post :index, :project_id => 'redmine', :payload => @json
+    post :index, :project_id => 'redmine', :payload => json
   end
 
   def test_should_downcase_identifier
     # Redmine project identifiers are always downcase
-    Project.expects(:find_by_identifier).with('redmine').returns(@project)
+    Project.expects(:find_by_identifier).with('redmine').returns(project)
     @controller.stubs(:exec).returns(true)
-    post :index, :project_id => 'ReDmInE', :payload => @json
+    post :index, :project_id => 'ReDmInE', :payload => json
   end
 
   def test_should_render_ok_when_done
@@ -121,7 +132,7 @@ class GithubHookControllerTest < ActionController::TestCase
 
   def test_should_fetch_changesets_into_the_repository
     @controller.expects(:update_repository).returns(true)
-    @repository.expects(:fetch_changesets).returns(true)
+    repository.expects(:fetch_changesets).returns(true)
 
     do_post
     assert_response :success
