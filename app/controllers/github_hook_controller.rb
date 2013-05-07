koppen/redmine_github_hook

commit 2c836e6f2c30d341076ff04477cec64b569d383e

Allow one-to-many from projects to repos
minor update

app/controllers/github_hook_controller.rb
@@ -6,13 +6,15 @@ class GithubHookController < ApplicationController
 
   def index
     if request.post?
-      repository = find_repository
+      repositories = find_repositories
 
-      # Fetch the changes from Github
-      update_repository(repository)
+      repositories.each do |repository|
+        # Fetch the changes from Github
+        update_repository(repository)
 
-      # Fetch the new changesets into Redmine
-      repository.fetch_changesets
+        # Fetch the new changesets into Redmine
+        repository.fetch_changesets
+      end
     end
 
     render(:text => 'OK')
@@ -76,12 +78,14 @@ class GithubHookController < ApplicationController
   end
 
   # Returns the Redmine Repository object we are trying to update
-  def find_repository
+  def find_repositories
     project = find_project
-    repository = project.repository
-    raise TypeError, "Project '#{project.to_s}' ('#{project.identifier}') has no repository" if repository.nil?
-    raise TypeError, "Repository for project '#{project.to_s}' ('#{project.identifier}') is not a Git repository" unless repository.is_a?(Repository::Git)
-    return repository
+    repositories = project.repositories
+    repositories.each do |repository|
+      raise TypeError, "Project '#{project.to_s}' ('#{project.identifier}') has no repository" if repository.nil?
+      raise TypeError, "Repository for project '#{project.to_s}' ('#{project.identifier}') is not a Git repository" unless repository.is_a?(Repository::Git)
+    end
+    return repositories
   end
 
 end

commit 455e031c4a897c7f211deda0e0ca3a087c7e0f7b

Make tests pass again
Broken in commit 2c836e6f2c30d341076ff04477cec64b569d383e, when support for multiple repositories was added.

We really need an easier way to get tests running, perhaps even set up on Travis?

app/controllers/github_hook_controller.rb
@@ -84,7 +84,7 @@ class GithubHookController < ApplicationController
   # Returns the Redmine Repository object we are trying to update
   def find_repositories
     project = find_project
-    repositories = project.repositories.all.select do |repo|
+    repositories = project.repositories.select do |repo|
         repo.is_a?(Repository::Git)
     end
 

test/functional/github_hook_controller_test.rb
@@ -47,12 +47,13 @@ class GithubHookControllerTest < ActionController::TestCase
       "after": "de8251ff97ee194a289832576287d6f8ad74e3d0",
       "ref": "refs/heads/master"
     }'
+
     @repository = Repository::Git.new
     @repository.stubs(:fetch_changesets).returns(true)
-    @repository.stubs(:url).returns('/path/to/somewhere/on/the/local/filesystem/.git')
 
     @project = Project.new
-    @project.stubs(:repository).returns(@repository)
+    @project.repositories << @repository
+
     Project.stubs(:find_by_identifier).with('github').returns(@project)
 
     # Make sure we don't run actual commands in test
@@ -121,6 +122,7 @@ class GithubHookControllerTest < ActionController::TestCase
   def test_should_fetch_changesets_into_the_repository
     @controller.expects(:update_repository).returns(true)
     @repository.expects(:fetch_changesets).returns(true)
+
     do_post
     assert_response :success
     assert_equal 'OK', @response.body
@@ -142,7 +144,7 @@ class GithubHookControllerTest < ActionController::TestCase
   def test_should_return_500_if_project_has_no_repository
     assert_raises TypeError do
       project = mock('project', :to_s => 'My Project', :identifier => 'github')
-      project.expects(:repository).returns(nil)
+      project.expects(:repositories).returns([])
       Project.expects(:find_by_identifier).with('github').returns(project)
       do_post :repository => {:name => 'github'}
     end
@@ -152,7 +154,7 @@ class GithubHookControllerTest < ActionController::TestCase
     assert_raises TypeError do
       project = mock('project', :to_s => 'My Project', :identifier => 'github')
       repository = Repository::Subversion.new
-      project.expects(:repository).at_least(1).returns(repository)
+      project.expects(:repositories).at_least(1).returns([repository])
       Project.expects(:find_by_identifier).with('github').returns(project)
       do_post :repository => {:name => 'github'}
     end