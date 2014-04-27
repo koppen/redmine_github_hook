f3659bf95385988c1f89884652c2b15ecf60d649
Jakob Skjerning <jakob@mentalized.net>
2014-04-27 15:04:37 +0200

    Release version 2.0.0
    This plugin has been around for a while and in production in many Redmine
    installations. The current 0.2.0 version didn't do this justice.
    
    Bumping all the way to 2.x to signal the compatibility with Redmines 2.x line.
    Going forward major version numbers should follow Redmines major versions.

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :redmine_github_hook do
   name 'Redmine Github Hook plugin'
   author 'Jakob Skjerning'
   description 'This plugin allows your Redmine installation to receive Github post-receive notifications'
-  version '0.2.0'
+  version '2.0.0'
 end
