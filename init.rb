d3e66c281a68eeb95b322dc1b255a5c719e1d87f
Jakob Skjerning <jakob@mentalized.net>
2014-05-03 14:40:08 +0200

    Correct author URL [ci skip]

--- init.rb
+++ init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :redmine_github_hook do
   author 'Jakob Skjerning'
   description 'This plugin allows your Redmine installation to receive Github post-receive notifications'
   url 'https://github.com/koppen/redmine_github_hook'
-  author_url 'https://github.com/koppen/'
+  author_url 'http://mentalized.net'
   version RedmineGithubHook::VERSION
 end
