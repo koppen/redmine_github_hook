9444cdab4bb125313dcde0665d41c9724609a5e0
Tobias Fischer <tobias.carl.fischer@gmail.com>
2014-05-03 11:15:26 +0200

    add plugin URL and author URL

--- init.rb
+++ init.rb
@@ -3,6 +3,8 @@ require 'redmine'
 Redmine::Plugin.register :redmine_github_hook do
   name 'Redmine Github Hook plugin'
   author 'Jakob Skjerning'
-  description 'This plugin allows your Redmine installation to receive Github post-receive notifications'
+  description 'Allows your Redmine installation to receive Github post-receive notifications'
+  url 'https://github.com/koppen/redmine_github_hook'
+  author_url 'http://mentalized.net'
   version RedmineGithubHook::VERSION
 end
